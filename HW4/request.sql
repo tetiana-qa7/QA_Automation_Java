779d3985274e457ded6b6f7bd12afad8381ec5f3
Tetiana <tetianapulia@gmail.com>
2024-06-19 17:31:30 +0300

    additional task done
    SELECT with JON

---
+++ HW4/request.sql
@@ -0,0 +1,13 @@
+/* візити які існують в базі та які відбулися виключно після 1 Березня 2024 року, 
+у видачі для кожного візиту має повернутися його дата, 
+причина візиту, виставлений діагноз, 
+призначене лікування, ім*я та порода котика, 
+а також ім*я та телефонний номер хазяїна котика (все саме в такому порядку) */
+
+
+SELECT VisitDate, Reason, Diagnosis, Treatment, Cats.Name, Breed FROM Cats
+JOIN Visits
+ON Cats.CatID = Visits.CatID
+JOIN MedicalRecords
+ON Visits.VisitID = MedicalRecords.`VisitID`
+WHERE visitDate > 2024-03-01;
